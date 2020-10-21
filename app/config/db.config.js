27b35e821347da86ede41966d7306bd34318a49e
Hugo Nieto <hugocrony@gmail.com>
2020-10-20 22:43:46 -0500

    fix name of db2

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "inventory-management.chasmnxxanut.eu-west-2.rds.amazonaws.com", // 127.0.0.1
-  USER: "admin", // root
-  PASSWORD: "Amarganale1988=", //amarganale1988
-  DB: "inventory-management",
+  HOST: "inventory-management.chasmnxxanut.eu-west-2.rds.amazonaws.com", // 127.0.0.1  // inventory-management.chasmnxxanut.eu-west-2.rds.amazonaws.com
+  USER: "admin", // root // admin
+  PASSWORD: "Amarganale1988=", //amarganale1988  //Amarganale1988=
+  DB: "inventoryManagement",
   dialect: "mysql",
   pool: {
     max: 5,
